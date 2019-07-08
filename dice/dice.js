2451561b9f544eec1a8de6e03fe4f8cc40cc569b
syedmuhammadmoin <46587349+syedmuhammadmoin@users.noreply.github.com>
2019-07-08 14:34:34 +0500

    Update dice.js

--- dice/dice.js
+++ dice/dice.js
@@ -5,29 +5,6 @@ function getRandom() {
 }
 
 
-
-// randomNum1 = getRandom();
-// randomNum2 = getRandom();
-// var randomNum1 =  (Math.floor(Math.random(6) * 6))  + 1   ;
-// var randomNum2 =  (Math.floor(Math.random(6) * 6))  + 1   ;
-
-// var imageLink =  "images/dice" + randomNum+ ".png";
-
-
-// console.log(randomNum);
-
-
-
-
-
-
-
-
-// console.log(imageLink);
-
-
-
-
 document.querySelector(".img1").src = "images/dice" + getRandom() + ".png";
 
 document.querySelector(".img2").src = "images/dice" + getRandom() + ".png";
